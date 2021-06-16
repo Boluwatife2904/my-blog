676315295dcac9bfc8da3decf3a158498b84b1c7
Boluwatife2904 <sanvicola2000@gmail.com>
2021-06-16 14:18:53 +0100

    Firebase and firestore setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoXijdlOePzVilBqOrSGVEfvfahXSBfjI",
+  authDomain: "vue-blog-7f602.firebaseapp.com",
+  projectId: "vue-blog-7f602",
+  storageBucket: "vue-blog-7f602.appspot.com",
+  messagingSenderId: "679651550081",
+  appId: "1:679651550081:web:0bb65a8e0c9f240a2abe80"
+};
+
+// Initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize firestore service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore }
