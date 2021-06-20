090a39d6efa6636d839fcb7ab87a116fd7c45d81
Boluwatife2904 <sanvicola2000@gmail.com>
2021-06-20 16:59:08 +0100

    Added deleting of posts functionality and modal

---
+++ src/components/DeleteModal.vue
@@ -0,0 +1,136 @@
+<template>
+  <teleport to="body">
+    <div class="modal-overlay" v-if="show">
+      <div class="modal">
+        <button class="close-modal" @click="toggleDeleteModal">
+          <i class="bx bx-x"></i>
+        </button>
+        <div class="modal-content">
+          <div class="close-icon">
+            <i class="bx bx-x-circle"></i>
+          </div>
+          <div class="modal-message">
+            <p class="header">Are you sure?</p>
+            <p class="message">
+              Do you really want to delete this record? This process cannot be
+              undone.
+            </p>
+          </div>
+          <div class="modal-actions">
+            <button @click="toggleDeleteModal" :disabled="loading">Cancel</button>
+            <button @click="performDeleteAction">
+              <i
+                class="bx bx-loader bx-spin bx-flip-horizontal"
+                v-if="loading"
+              ></i>
+              <span v-else> Delete</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: "DeleteModal",
+  props: ["show", "loading"],
+  emits: ["toggle-modal", "delete-post"],
+  setup(_, context) {
+    const toggleDeleteModal = () => {
+      context.emit("toggle-modal");
+    };
+
+    const performDeleteAction = () => {
+      context.emit("delete-post");
+    };
+
+    return { toggleDeleteModal, performDeleteAction };
+  },
+};
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.7);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #fff;
+  padding: 30px;
+  border-radius: 6px;
+  max-width: 400px;
+  width: 95%;
+  position: relative;
+}
+
+.close-modal {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background: none;
+  border: none;
+  outline: none;
+  font: inherit;
+  font-size: 25px;
+  color: rgb(124, 124, 124);
+  cursor: pointer;
+  padding: 10px;
+}
+
+.modal-content {
+  text-align: center;
+}
+
+.modal-content .close-icon i {
+  font-size: 60px;
+  color: rgb(255, 79, 114);
+}
+
+.modal-content .modal-message {
+  margin-bottom: 20px;
+}
+
+.modal-content .modal-message .header {
+  font-size: 24px;
+  font-weight: 500;
+  color: rgb(65, 65, 65);
+}
+
+.modal-content .modal-message .message {
+  font-size: 15px;
+  line-height: 22px;
+  color: rgb(65, 65, 65);
+}
+
+.modal-content .modal-actions button {
+  width: 130px;
+  max-width: 90%;
+  padding: 13px 30px;
+  border: none;
+  outline: none;
+  margin: 5px;
+  text-align: center;
+  font: inherit;
+  border-radius: 4px;
+  cursor: pointer;
+  color: #fff;
+}
+
+.modal-content .modal-actions button:first-child {
+  background: rgb(199, 199, 199);
+}
+
+.modal-content .modal-actions button:last-child {
+  background: rgb(255, 79, 114);
+}
+</style>
--- src/views/BlogDetails.vue
+++ src/views/BlogDetails.vue
@@ -1,25 +1,53 @@
 <template>
   <div class="post">
+    <DeleteModal 
+      @toggle-modal="toggleDeleteModal" 
+      @delete-post="deleteBlogPost" 
+      :show="showModal"
+      :loading="actionInProgress"
+      />
     <Spinner v-if="isLoading" />
     <div v-else-if="!isLoading && post">
       <h3>{{ post.title }}</h3>
       <p class="pre">{{ post.body }}</p>
       <span v-for="tag in post.tags" :key="tag" class="tag-item">#{{ tag }}</span>
+      <button class="delete" @click="toggleDeleteModal">Delete Post</button>
     </div>
     <div v-else>{{ error }}</div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 import Spinner from "../components/Spinner.vue";
+import DeleteModal from "../components/DeleteModal.vue"
 import getSinglePost from "../composables/getSinglePost";
+import { projectFirestore } from "../firebase/config";
+import { useRouter } from "vue-router";
+
 export default {
-  components: { Spinner },
+  components: { Spinner, DeleteModal },
   props: ["id"],
   setup(props) {
+    const router = useRouter();
+    const showModal = ref(false);
+    const actionInProgress = ref(false);
     const { post, error, isLoading, loadData } = getSinglePost(props.id);
     loadData();
-    return { post, error, isLoading };
+
+    const toggleDeleteModal = () => {
+      showModal.value = !showModal.value;
+    }
+
+    const deleteBlogPost = async () => {
+      actionInProgress.value = true;
+      await projectFirestore.collection("posts").doc(props.id).delete();
+      actionInProgress.value = false;
+      router.replace({ name: "Home"})
+      console.log("Blog Post Deleted Successfully!!!")
+    }
+
+    return { post, error, isLoading, showModal, toggleDeleteModal, deleteBlogPost, actionInProgress };
   },
 };
 </script>
@@ -38,5 +66,17 @@ export default {
 
 .post .pre {
   white-space: pre-wrap;
+}
+
+.delete {
+  border: none;
+  border-radius: 6px;
+  padding: 12px 20px;
+  display: block;
+  color: #fff;
+  background-color: #ff8800;
+  font: inherit;
+  cursor: pointer;
+  margin: 30px 0;
 }
 </style>
